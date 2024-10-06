6c2cf1b5411817d93850c5f52f16f482da299c0a
Kenzimonster <154712258+Kenzimonster@users.noreply.github.com>
2024-10-06 16:56:31 +1100

    finishing touches
    added a few more poerty type options.
    made the type select have a scroll when focused.
    minor changes to css styling
    and a few tweets to the AI context prompt to get better responses from the AI

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event){
   let poemType = document.querySelector("#poemType");
   let apiKey = "3at0foeb77eba84a5c21cf21f38b13e9";
   let prompt =`User Instrcution: generate a ${poemType.value} poem about ${instructionsInput.value} in the traditional format of ${poemType.value}`; 
-  let context = `you are an expert in poetry and know many types of poetry and must wirte each type of poetry in their traditional styling, your mission is to generate a short poem keeping strictly to the traditional formating of ${poemType.value} poetry, seperate each line with a <br/>. make sure to follow the User Instrucion. please do not include a title and please sign the poem "the Goblins" at the end of the poem in a <em> element and please do NOT sign at the begining `;
+  let context = `you are an expert in poetry and know many types of poetry and must wirte each type of poetry in their traditional styling, your mission is to generate a short poem keeping strictly to the traditional formating of ${poemType.value} poetry, please seperate each line with a <br/> and seperate each stanza into <p> elemenets. make sure to follow the User Instrucion. please do not include a title and please sign the poem "the Goblins" at the end of the poem in a <em> element and please do NOT sign at the begining `;
   let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
   let poemElement = document.querySelector("#aiResponse");
   poemElement.classList.remove("hidden")
