a4dbdae181839388251e9d9fc1e3d42aa0a13473
Kenzimonster <154712258+Kenzimonster@users.noreply.github.com>
2024-10-06 15:50:43 +1100

    improved user experience
    made user experience better by adding loading text and and examples of topics,
    created if statement to remind user to select a poem type if they forget.
    added css for new elements and neatened the code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response){
-  console.log("Poem complete")
   new Typewriter("#aiResponse", {
     strings: response.data.answer,
     autoStart: true,
@@ -9,26 +8,21 @@ function displayPoem(response){
 }
 
 function generatePoem(event){
- event.preventDefault();
- let instructionsInput = document.querySelector("#userInput");
- let poemType = document.querySelector("#poemType");
- let apiKey = "3at0foeb77eba84a5c21cf21f38b13e9";
- let prompt =`User Instrcution: generate a ${poemType.value} poem about ${instructionsInput.value} in the traditional format of ${poemType.value}`; 
- let context = `you are an expert in poetry and know many types of poetry and must wirte each type of poetry in their traditional styling, your mission is to generate a short poem keeping strictly to the traditional formating of ${poemType.value} poetry, seperate each line with a <br/>. make sure to follow the User Instrucion. please do not include a title and please sign the poem "the Goblins" at the end of the poem in a <em> element and please do NOT sign at the begining `;
- let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
- console.log("generating poem")
- console.log(`prompt: ${prompt}`)
- console.log(`context: ${context}`)
- axios.get(apiUrl).then(displayPoem);
-}
-
-function loading(){
-  new Typewriter("#aiResponse", {
-    strings: "The goblins are crafting your poem",
-    autoStart: true,
-    delay: 1,
-    cursor: " ",
-  });
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#userInput");
+  let poemType = document.querySelector("#poemType");
+  let apiKey = "3at0foeb77eba84a5c21cf21f38b13e9";
+  let prompt =`User Instrcution: generate a ${poemType.value} poem about ${instructionsInput.value} in the traditional format of ${poemType.value}`; 
+  let context = `you are an expert in poetry and know many types of poetry and must wirte each type of poetry in their traditional styling, your mission is to generate a short poem keeping strictly to the traditional formating of ${poemType.value} poetry, seperate each line with a <br/>. make sure to follow the User Instrucion. please do not include a title and please sign the poem "the Goblins" at the end of the poem in a <em> element and please do NOT sign at the begining `;
+  let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+  let poemElement = document.querySelector("#aiResponse");
+  poemElement.classList.remove("hidden")
+  if (poemType.value){
+    poemElement.innerHTML = `<p class="generating">✍🏽The goblins are crafting a ${poemType.value} about ${instructionsInput.value} for you✍🏽</p>`
+    axios.get(apiUrl).then(displayPoem);
+  }else{
+   poemElement.innerHTML ="Please select a type of poem from the list to help the goblins craft your poem"
+   }
 }
 
 let poemFormElement = document.querySelector("#poemGenerator")
